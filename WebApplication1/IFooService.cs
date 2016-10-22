26156c030a1b2100fd208aa31e8fe3bd48c26c5c
Chris Gomez <cagomez@cellbycell.com>
2016-10-22 09:16:19 -0400

    Define rest of FooService interface
    Defined the other methods we'll need.  Just figured I'd do it all at once.

--- WebApplication1/IFooService.cs
+++ WebApplication1/IFooService.cs
@@ -3,5 +3,8 @@
     public interface IFooService
     {
         object GetBaz();
+        object PostBaz(string value);
+        object PutBaz(string value);
+        object GetABaz(int id);
     }
 }
